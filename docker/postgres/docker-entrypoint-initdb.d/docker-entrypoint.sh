57df8651abf77a0d568a73d6e2763fc7e0443c0f
equal <equal@KRIIIK.local>
2024-04-15 19:23:35 +0500

    Debug

--- docker/postgres/docker-entrypoint-initdb.d/docker-entrypoint.sh
+++ docker/postgres/docker-entrypoint-initdb.d/docker-entrypoint.sh
@@ -5,5 +5,5 @@
    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
        CREATE USER myuser WITH PASSWORD 'mypassword';
        CREATE DATABASE mydb;
-       GRANT ALL PRIVILEGES ON DATABASE myshop_db TO myuser;
+       GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
    EOSQL
