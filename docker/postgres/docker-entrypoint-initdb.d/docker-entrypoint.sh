a05eeceb6638b5de4a19ca59f9ff63f3a08de774
3qual <mncqqq@proton.me>
2024-07-19 20:19:33 +0500

    init

--- docker/postgres/docker-entrypoint-initdb.d/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-!/bin/bash
-
-
-
-   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-       CREATE USER myuser WITH PASSWORD 'mypassword';
-       CREATE DATABASE mydb;
-       GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
-   EOSQL
